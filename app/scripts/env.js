622a30606a32605b855db8a1ac066a53e4c78c33
Joel <jwang2@apdgroup.com>
2017-08-24 01:09:18 +0800

    couchdb server from https://cloudno.de/

--- app/scripts/env.js
+++ app/scripts/env.js
@@ -14,8 +14,8 @@ var env = {
     // apiHost: 'https://app.shopeur.com.staging-2.oneba.se',
     apiHost: 'https://bitbucket.org/!api/2.0/snippets/JoelPub',
     apiPort: null,
-    couchDBHost: 'https://couchdb.shopeur.com.staging-2.oneba.se',
-    // couchDBHost: 'https://bitbucket.org/!api/2.0/snippets/JoelPub',
+    // couchDBHost: 'https://couchdb.shopeur.com.staging-2.oneba.se',
+    couchDBHost: 'https://couchdb.cloudno.de',
     couchDBPort: null,
     couchDBName: 'shopeur'
     // couchDBName: '7RaL5a/9584bfd3446b4f67ea65294636eb0df73197b5f2/files/snippet.json'
